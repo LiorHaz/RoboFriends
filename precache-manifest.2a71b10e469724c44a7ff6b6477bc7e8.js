bcd3f073ba970f9c980155d734e42dfbb451242c
LiorHaz <lior1310@gmail.com>
2020-09-30 17:01:16 +0300

    Updates

---
+++ precache-manifest.2a71b10e469724c44a7ff6b6477bc7e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5cada7ac79fbbb9e25ba",
+    "url": "/robofriends/static/css/main.1e619dd7.chunk.css"
+  },
+  {
+    "revision": "5cada7ac79fbbb9e25ba",
+    "url": "/robofriends/static/js/main.de3dc285.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "03499223d4c027f86c50",
+    "url": "/robofriends/static/css/2.c23efb93.chunk.css"
+  },
+  {
+    "revision": "03499223d4c027f86c50",
+    "url": "/robofriends/static/js/2.eccda3e4.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "7ca3158dee8610db5699e46e652dc69f",
+    "url": "/robofriends/index.html"
+  }
+];
